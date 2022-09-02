c494f2e57c291a1148ec7d765f2a36b30f363324
ViktarYakimuk <ViktarYakimuk>
2022-09-02 16:22:18 +0300

    вычисления модуля  числа.

---
+++ pra006/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу вычисления модуля  числа.
+//2 -> 2
+//-3 -> 3
+//-7 -> 7
+
+int Prompt(string message)
+{
+   Console.Write("Введите число > ");          // вывод приглашения.
+   string strValue;                      // обьявдения для ввода строки
+   strValue = Console.ReadLine() ?? "0"; // вводими строку в консоли.(с консоли можно ввести только строку.)
+   int value = int.Parse(strValue);      // преоброзование строки в целое число.
+   return value;
+}
+int value = Prompt("Введите число -> ");
+int result;
+if (value < 0)
+{
+   result = -value;
+}
+else
+{
+   result = value;
+}
+System.Console.WriteLine($"{value} ->{result}");
