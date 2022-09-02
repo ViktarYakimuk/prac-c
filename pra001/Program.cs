942020d1d8a57aab4c1797335c9d24831b2cd66b
ViktarYakimuk <ViktarYakimuk>
2022-09-02 10:13:34 +0300

    выдает квадрат.

---
+++ pra001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+//Ввод числа из консоли по приглашению
+int Prompt(string message)
+{
+   Console.Write("message > ");          // вывод приглашения.
+   string strValue;                      // обьявдения для ввода строки
+   strValue = Console.ReadLine() ?? "0"; // вводими строку в консоли.(с консоли можно ввести только строку.)
+   int value = int.Parse(strValue);      // преоброзование строки в целое число.
+   return value;
+}
+int value = Prompt("Введите, число = ");
+int result = value * value;          // Вычесление квадрата (заносим в переменную result)
+System.Console.WriteLine($"Квадрат числа {value} равен {result} "); // Вывод.
+
+
