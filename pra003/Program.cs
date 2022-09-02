d571c04f61b56d3b70238563b73bf766e5b9579f
ViktarYakimuk <ViktarYakimuk>
2022-09-02 14:30:25 +0300

    дни недели.

---
+++ pra003/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+int Prompt(string message)
+{
+   Console.Write("message > ");          // вывод приглашения.
+   string strValue;                      // обьявдения для ввода строки
+   strValue = Console.ReadLine() ?? "0"; // вводими строку в консоли.(с консоли можно ввести только строку.)
+   int value = int.Parse(strValue);      // преоброзование строки в целое число.
+   return value;
+}
+
+string[] days = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+int value = Prompt("Введите день недели >");
+int daysValue = value - 1;
+System.Console.WriteLine($"{value}->{days[daysValue]}");
